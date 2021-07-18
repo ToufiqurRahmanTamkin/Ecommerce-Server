96b2b3825daa84dea47e095354463d26ee5a9ce7
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-18 16:56:34 +0600

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
-import express from "express";
-import data from "./data.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
-const port = process.env.PORT || 5000;
+mongoose.connect('mongodb+srv://tamkin:tamkin12345@cluster0.rmrod.mongodb.net/ecommerce?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.use('/api/users', userRouter);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });

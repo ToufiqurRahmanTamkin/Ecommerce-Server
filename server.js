329d0a6bbf7e303967a089b23a5b30b505019ce8
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-19 01:56:10 +0600

    create route and add products to database

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import data from './data.js';
-import userRouter from './routers/userRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
 
 const app = express();
-mongoose.connect('mongodb+srv://tamkin:tamkin12345@cluster0.rmrod.mongodb.net/ecommerce?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
+mongoose.connect(
+  "mongodb+srv://tamkin:tamkin12345@cluster0.rmrod.mongodb.net/ecommerce?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
   }
-});
+);
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.use('/api/users', userRouter);
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
 app.use((err, req, res, next) => {

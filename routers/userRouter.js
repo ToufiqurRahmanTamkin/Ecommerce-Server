60ed307650a01b5ee1e66bbd238db21ca0013e10
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-20 02:54:55 +0600

    create order route

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,14 +1,14 @@
-import express from "express";
-import expressAsyncHandler from "express-async-handler";
-import bcrypt from "bcryptjs";
-import data from "../data.js";
-import User from "../models/userModel.js";
-import { generateToken } from "../utils.js";
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import data from '../data.js';
+import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
 userRouter.get(
-  "/seed",
+  '/seed',
   expressAsyncHandler(async (req, res) => {
     // await User.remove({});
     const createdUsers = await User.insertMany(data.users);
@@ -17,7 +17,7 @@ userRouter.get(
 );
 
 userRouter.post(
-  "/signin",
+  '/signin',
   expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
@@ -32,7 +32,26 @@ userRouter.post(
         return;
       }
     }
-    res.status(401).send({ message: "Invalid email or password" });
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+userRouter.post(
+  '/register',
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: createdUser.isAdmin,
+      token: generateToken(createdUser),
+    });
   })
 );
 
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import productRouter from "./routers/productRouter.js";
-import userRouter from "./routers/userRouter.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import productRouter from './routers/productRouter.js';
+import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
 dotenv.config();
 
@@ -10,19 +11,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(
-  "mongodb+srv://tamkin:tamkin12345@cluster0.rmrod.mongodb.net/ecommerce?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-app.use("/api/users", userRouter);
-app.use("/api/products", productRouter);
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
 });
 
 app.use((err, req, res, next) => {

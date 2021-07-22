22ccbe710dea5c1a05a2571410b3ca46d82c520a
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-22 14:08:14 +0600

    create ordera api and order model

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import productRouter from './routers/productRouter.js';
-import userRouter from './routers/userRouter.js';
-import orderRouter from './routers/orderRouter.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
 
@@ -11,17 +11,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
 
-app.use('/api/users', userRouter);
-app.use('/api/products', productRouter);
-app.use('/api/orders', orderRouter);
-app.get('/', (req, res) => {
-  res.send('Server is ready');
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
 });
 
 app.use((err, req, res, next) => {

57c6f90f2f1000c0607ac4ed79859b38a88319bb
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-26 18:42:50 +0600

    create delete api for products and also create api for order list

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,10 +1,18 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
 import Order from "../models/orderModel.js";
-import { isAuth } from "../utils.js";
+import { isAdmin, isAuth } from "../utils.js";
 
 const orderRouter = express.Router();
-
+orderRouter.get(
+  "/",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({}).populate("user", "name");
+    res.send(orders);
+  })
+);
 orderRouter.get(
   "/mine",
   isAuth,
@@ -68,6 +76,21 @@ orderRouter.put(
       };
       const updatedOrder = await order.save();
       res.send({ message: "Order Paid", order: updatedOrder });
+    } else {
+      res.status(404).send({ message: "Order Not Found" });
+    }
+  })
+);
+
+orderRouter.delete(
+  "/:id",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      const deleteOrder = await order.remove();
+      res.send({ message: "Order Deleted", order: deleteOrder });
     } else {
       res.status(404).send({ message: "Order Not Found" });
     }

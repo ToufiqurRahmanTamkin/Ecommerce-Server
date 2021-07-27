07c609aec5d53909ae929252d9e88e3ae68bbe15
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-28 03:03:11 +0600

    update order model, order router, product router and userrouter

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,6 +8,16 @@ import { generateToken, isAdmin, isAuth } from "../utils.js";
 const userRouter = express.Router();
 
 userRouter.get(
+  "/top-sellers",
+  expressAsyncHandler(async (req, res) => {
+    const topSellers = await User.find({ isSeller: true })
+      .sort({ "seller.rating": -1 })
+      .limit(3);
+    res.send(topSellers);
+  })
+);
+
+userRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
     // await User.remove({});
@@ -136,8 +146,9 @@ userRouter.put(
     if (user) {
       user.name = req.body.name || user.name;
       user.email = req.body.email || user.email;
-      user.isSeller = req.body.isSeller || user.isSeller;
-      user.isAdmin = req.body.isAdmin || user.isAdmin;
+      user.isSeller = Boolean(req.body.isSeller);
+      user.isAdmin = Boolean(req.body.isAdmin);
+      // user.isAdmin = req.body.isAdmin || user.isAdmin;
       const updatedUser = await user.save();
       res.send({ message: "User Updated", user: updatedUser });
     } else {

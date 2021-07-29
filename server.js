cee14c6b8acd72659dfd497985e9743ee3159b5c
Toufiqur Rahman Tamkin <tamkin22rahman@gmail.com>
2021-07-29 16:19:39 +0600

    server update

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get("/api/config/google", (req, res) => {
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
